d711de986b400f1432e6f13d4a3923f3e8d4943d
ZhuravlevAleksandr <zhurzhurych@gmail.com>
2022-05-27 17:04:16 +0300

    Добавляет UserProfileRestController для обновления профиля юзера

---
+++ src/main/java/kz/kaitanov/fitnessbackend/model/dto/request/UserUpdateProfileDto.java
@@ -0,0 +1,14 @@
+package kz.kaitanov.fitnessbackend.model.dto.request;
+
+import kz.kaitanov.fitnessbackend.model.enums.Gender;
+import javax.validation.constraints.Positive;
+
+public record UserUpdateProfileDto(
+        @Positive Long id,
+        String firstName,
+        String lastName,
+        String email,
+        String phone,
+        Integer age,
+        Gender gender) {
+}
