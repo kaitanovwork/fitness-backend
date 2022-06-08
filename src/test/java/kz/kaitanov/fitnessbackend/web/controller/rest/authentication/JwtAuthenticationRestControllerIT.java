22764ebc979c34d5515058da168637fb434c619f
ZhuravlevAleksandr <zhurzhurych@gmail.com>
2022-06-08 17:12:46 +0300

    Добавляет тесты в JwtAuthenticationRestControllerIT

--- src/test/java/kz/kaitanov/fitnessbackend/web/controller/rest/authentication/JwtAuthenticationRestControllerIT.java
+++ src/test/java/kz/kaitanov/fitnessbackend/web/controller/rest/authentication/JwtAuthenticationRestControllerIT.java
@@ -3,6 +3,7 @@ package kz.kaitanov.fitnessbackend.web.controller.rest.authentication;
 import kz.kaitanov.fitnessbackend.SpringSimpleContextTest;
 import kz.kaitanov.fitnessbackend.model.dto.request.JwtRequest;
 import org.hamcrest.core.Is;
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
@@ -30,6 +31,42 @@ public class JwtAuthenticationRestControllerIT extends SpringSimpleContextTest {
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(true)))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(200)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data").isMap());
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.jwtToken").value(StringContains.containsString("ey")));
+    }
+
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+            value = "/scripts/authentication/JwtAuthenticationRestController/сreateAuthenticationToken_UsernameNotFoundTest/BeforeTest.sql")
+    @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+            value = "/scripts/authentication/JwtAuthenticationRestController/сreateAuthenticationToken_UsernameNotFoundTest/AfterTest.sql")
+    public void сreateAuthenticationToken_UsernameNotFoundTest() throws Exception {
+
+        JwtRequest jwtRequest = new JwtRequest("user1", "pass");
+
+        mockMvc.perform(post("/api/v1/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(jwtRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(false)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(400)));
+    }
+
+    @Test
+    @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+            value = "/scripts/authentication/JwtAuthenticationRestController/сreateAuthenticationToken_PasswordNotFoundTest/BeforeTest.sql")
+    @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+            value = "/scripts/authentication/JwtAuthenticationRestController/сreateAuthenticationToken_PasswordNotFoundTest/AfterTest.sql")
+    public void сreateAuthenticationToken_PasswordNotFoundTest() throws Exception {
+
+        JwtRequest jwtRequest = new JwtRequest("username", "password");
+
+        mockMvc.perform(post("/api/v1/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(jwtRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success", Is.is(false)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code", Is.is(400)));
     }
 }
