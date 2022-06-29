3f77b5353118624b3a07c84d08ffff240b49b0f1
Vadim Lapshin <lapshin_va@list.ru>
2022-06-29 18:12:25 +0300

    Создает обновление токена без отправки логина и пароля

--- src/main/java/kz/kaitanov/fitnessbackend/web/controller/rest/authentication/JwtAuthenticationRestController.java
+++ src/main/java/kz/kaitanov/fitnessbackend/web/controller/rest/authentication/JwtAuthenticationRestController.java
@@ -8,13 +8,18 @@ import kz.kaitanov.fitnessbackend.model.dto.request.JwtRequest;
 import kz.kaitanov.fitnessbackend.model.dto.response.JwtResponse;
 import kz.kaitanov.fitnessbackend.model.dto.response.api.Response;
 import kz.kaitanov.fitnessbackend.web.config.util.JwtTokenUtil;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +44,33 @@ public class JwtAuthenticationRestController {
         authenticate(jwtRequest.username(), jwtRequest.password());
         final UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.username());
         return Response.ok(new JwtResponse(jwtTokenUtil.generateToken(userDetails)));
+    }
+
+    @Operation(summary = "Обновление токена")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Токен успешно обновлен")
+    })
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'USER', 'COACH')")
+    @PostMapping("/refresh")
+    public Response<JwtResponse> refreshToken(@RequestHeader("Authorization") String token) {
+        final UserDetails userDetails;
+        String jwtToken = null;
+        String username = null;
+
+        if (token != null && token.startsWith("Bearer ")) {
+            jwtToken = token.substring(7);
+            try {
+                username = jwtTokenUtil.getUsernameFromToken(jwtToken);
+            } catch (Exception ignored) {
+            }
+        }
+        userDetails = userDetailsService.loadUserByUsername(username);
+
+        if(!jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+            return Response.ok(new JwtResponse(jwtTokenUtil.generateToken(userDetails)));
+        } else {
+            return Response.ok(new JwtResponse(jwtToken));
+        }
     }
 
     private void authenticate(String username, String password) {
