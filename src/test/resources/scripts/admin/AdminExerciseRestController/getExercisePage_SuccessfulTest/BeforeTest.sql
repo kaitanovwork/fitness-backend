616ddb2be97bad5dd5a8a21401b1ea661ef9c626
armenchik-1 <armenchikgaloyan@yandex.ru>
2022-06-30 11:01:38 +0300

    Изменяет enum Area на новые значения и подправляет тесты

--- src/test/resources/scripts/admin/AdminExerciseRestController/getExercisePage_SuccessfulTest/BeforeTest.sql
+++ src/test/resources/scripts/admin/AdminExerciseRestController/getExercisePage_SuccessfulTest/BeforeTest.sql
@@ -1,8 +1,8 @@
 INSERT INTO exercises (id, approach_count, area, muscle_group, name, repeat_count)
-VALUES (101, 10, 'HOME', 'Biceps', 'TestExercise1', 10),
-       (102, 12, 'GYM', 'Legs', 'TestExercise2', 12),
-       (103, 8, 'HOME', 'Triceps', 'TestExercise3', 12),
-       (104, 6, 'GYM', 'Shoulders', 'TestExercise4', 15);
+VALUES (101, 10, 'ARMS', 'Biceps', 'TestExercise1', 10),
+       (102, 12, 'LEGS', 'Legs', 'TestExercise2', 12),
+       (103, 8, 'BREAST', 'Triceps', 'TestExercise3', 12),
+       (104, 6, 'CHEST', 'Shoulders', 'TestExercise4', 15);
 
 INSERT INTO roles (id, name)
 VALUES (101, 'ADMIN');
